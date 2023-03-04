857f6758a772216ed69dac394e94fec65b7e0805
Kerem Kahraman <kerem.kahraman001@gmail.com>
2023-03-04 20:17:06 +0300

    boot.rb changes remove

--- config/boot.rb
+++ config/boot.rb
@@ -1,5 +1,4 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
-ENV['RAILS_DISABLE_BOOTSNAP'] = '1'
 
 require 'bundler/setup' # Set up gems listed in the Gemfile.
 require 'bootsnap/setup' # Speed up boot time by caching expensive operations.
